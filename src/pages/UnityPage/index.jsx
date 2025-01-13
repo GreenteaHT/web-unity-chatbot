021cf8d7c2ff86daf203f0d6c96e10b655cc6ec2
GreenteaHT <sh0946@gmail.com>
2025-01-13 20:49:26 +0900

    feat(unityPage): Load and display Unity project on UnityPage
    Implemented UnityPage component to load and display the Unity WebGL project using react-unity-webgl.

--- src/pages/UnityPage/index.jsx
+++ src/pages/UnityPage/index.jsx
@@ -1,6 +1,17 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 function UnityPage() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "unityBuild/WebBuild.loader.js",
+    dataUrl: "unityBuild/WebBuild.data",
+    frameworkUrl: "unityBuild/WebBuild.framework.js",
+    codeUrl: "unityBuild/WebBuild.wasm",
+  });
+
   return (
-    <h1 className="text-xl">test</h1>
+    <div className="flex justify-center items-center w-screen h-screen">
+      <Unity unityProvider={unityProvider} className="w-4/6 h-4/6" />
+    </div>
   );
 }
 
