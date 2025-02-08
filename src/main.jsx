bfe4a3f45311ff8e17dd82274f1425a0f69dcd18
GreenteaHT <sh0946@gmail.com>
2025-02-08 17:59:13 +0900

    feat(router): Integrated react-router-dom into the main entry point
    Updated the main entry point to include BrowserRouter for enabling routing functionality.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import UnityPage from './pages/UnityPage/index.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UnityPage />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
