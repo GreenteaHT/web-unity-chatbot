78666b7b73a82964cec30e536ed22c65773f99b5
GreenteaHT <sh0946@gmail.com>
2025-02-13 11:49:00 +0900

    chore(utils): Add constants file for global usage
    Created a constants file in the utils folder to store globally used values, improving code organization and maintainability.

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+// image
+export const DEFAULT_IMAGE_URL = "https://www.gravatar.com/avatar/?d=mp";
+
+// text length limit
+export const MAX_CHARACTER_NAME_LENGTH = 20;
+export const MAX_CHARACTER_DESCRIPTION_LENGTH = 500;
+export const MAX_CHARACTER_BACKGROUND_LENGTH = 5000;
+export const MAX_DIALOGUE_USER_MESSAGE_LENGTH = 200;
+export const MAX_DIALOGUE_RESPONSE_LENGTH = 1000;
+export const MAX_LOREBOOK_TITLE_LENGTH = 100;
+export const MAX_LOREBOOK_CONTENT_LENGTH = 1000;
